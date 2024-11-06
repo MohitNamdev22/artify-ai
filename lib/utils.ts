e9ad52ba979aa3f15a79d5b4bdb67bc276e80c19
Mohit Namdev <namdevmohit0@gmail.com>
2024-11-07 00:19:37 +0530

    changed in util

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,10 @@
-/* eslint-disable prefer-const */
-/* eslint-disable no-prototype-builtins */
+/* eslint-disable no-console */
 import { type ClassValue, clsx } from "clsx";
 import qs from "qs";
 import { twMerge } from "tailwind-merge";
-
 import { aspectRatioOptions } from "@/constants";
 
+// UTILITY FUNCTIONS
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -13,21 +12,18 @@ export function cn(...inputs: ClassValue[]) {
 // ERROR HANDLER
 export const handleError = (error: unknown) => {
   if (error instanceof Error) {
-    // This is a native JavaScript error (e.g., TypeError, RangeError)
     console.error(error.message);
     throw new Error(`Error: ${error.message}`);
   } else if (typeof error === "string") {
-    // This is a string error message
     console.error(error);
     throw new Error(`Error: ${error}`);
   } else {
-    // This is an unknown type of error
     console.error(error);
     throw new Error(`Unknown error: ${JSON.stringify(error)}`);
   }
 };
 
-// PLACEHOLDER LOADER - while image is transforming
+// SHIMMER SVG FOR LOADING
 const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
@@ -39,7 +35,7 @@ const shimmer = (w: number, h: number) => `
   </defs>
   <rect width="${w}" height="${h}" fill="#7986AC" />
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite" />
 </svg>`;
 
 const toBase64 = (str: string) =>
@@ -50,9 +46,14 @@ const toBase64 = (str: string) =>
 export const dataUrl = `data:image/svg+xml;base64,${toBase64(
   shimmer(1000, 1000)
 )}`;
-// ==== End
 
-// FORM URL QUERY
+// FORM URL QUERY FUNCTION
+export interface FormUrlQueryParams {
+  searchParams: URLSearchParams;
+  key: string;
+  value: string;
+}
+
 export const formUrlQuery = ({
   searchParams,
   key,
@@ -65,18 +66,22 @@ export const formUrlQuery = ({
   })}`;
 };
 
-// REMOVE KEY FROM QUERY
+// REMOVE KEYS FROM QUERY
+export interface RemoveUrlQueryParams {
+  searchParams: URLSearchParams;
+  keysToRemove: string[];
+}
+
 export function removeKeysFromQuery({
   searchParams,
   keysToRemove,
 }: RemoveUrlQueryParams) {
-  const currentUrl = qs.parse(searchParams);
+  const currentUrl = qs.parse(searchParams.toString()) as Record<string, any>;
 
   keysToRemove.forEach((key) => {
     delete currentUrl[key];
   });
 
-  // Remove null or undefined values
   Object.keys(currentUrl).forEach(
     (key) => currentUrl[key] == null && delete currentUrl[key]
   );
@@ -84,20 +89,21 @@ export function removeKeysFromQuery({
   return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
 }
 
-// DEBOUNCE
+// DEBOUNCE FUNCTION
 export const debounce = (func: (...args: any[]) => void, delay: number) => {
-  let timeoutId: NodeJS.Timeout | null;
+  let timeoutId: ReturnType<typeof setTimeout> | null;
   return (...args: any[]) => {
     if (timeoutId) clearTimeout(timeoutId);
     timeoutId = setTimeout(() => func.apply(null, args), delay);
   };
 };
 
-// GE IMAGE SIZE
+// IMAGE SIZE FUNCTION
 export type AspectRatioKey = keyof typeof aspectRatioOptions;
+
 export const getImageSize = (
   type: string,
-  image: any,
+  image: { aspectRatio?: string; width?: number; height?: number },
   dimension: "width" | "height"
 ): number => {
   if (type === "fill") {
@@ -109,49 +115,37 @@ export const getImageSize = (
   return image?.[dimension] || 1000;
 };
 
-// DOWNLOAD IMAGE
-export const download = (url: string, filename: string) => {
-  if (!url) {
-    throw new Error("Resource URL not provided! You need to provide one");
+// DOWNLOAD FUNCTION
+export const download = async (url: string, filename: string) => {
+  if (!url) throw new Error("Resource URL not provided!");
+  
+  try {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    const blobURL = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = blobURL;
+    a.download = filename.replace(" ", "_") + ".png";
+    document.body.appendChild(a);
+    a.click();
+  } catch (error) {
+    console.error("Error downloading file:", error);
   }
-
-  fetch(url)
-    .then((response) => response.blob())
-    .then((blob) => {
-      const blobURL = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.href = blobURL;
-
-      if (filename && filename.length)
-        a.download = `${filename.replace(" ", "_")}.png`;
-      document.body.appendChild(a);
-      a.click();
-    })
-    .catch((error) => console.log({ error }));
 };
 
-// DEEP MERGE OBJECTS
-export const deepMergeObjects = (obj1: any, obj2: any) => {
-  if(obj2 === null || obj2 === undefined) {
-    return obj1;
-  }
+// DEEP MERGE FUNCTION
+export const deepMergeObjects = (obj1: Record<string, any>, obj2: Record<string, any>) => {
+  if (!obj2) return obj1;
 
-  let output = { ...obj2 };
+  const output = { ...obj2 };
 
   for (let key in obj1) {
-    if (obj1.hasOwnProperty(key)) {
-      if (
-        obj1[key] &&
-        typeof obj1[key] === "object" &&
-        obj2[key] &&
-        typeof obj2[key] === "object"
-      ) {
-        output[key] = deepMergeObjects(obj1[key], obj2[key]);
-      } else {
-        output[key] = obj1[key];
-      }
+    if (Object.prototype.hasOwnProperty.call(obj1, key)) {
+      output[key] =
+        obj1[key] && typeof obj1[key] === "object" && obj2[key] && typeof obj2[key] === "object"
+          ? deepMergeObjects(obj1[key], obj2[key])
+          : obj1[key];
     }
   }
-
   return output;
 };
