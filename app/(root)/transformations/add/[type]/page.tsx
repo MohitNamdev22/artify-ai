84022ae37ae8db1ed73a485882dd9a378a316fd1
Mohit Namdev <namdevmohit0@gmail.com>
2024-11-10 01:39:48 +0530

    changed the desctructuring way

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -2,7 +2,8 @@ import Header from '@/components/shared/Header'
 import { transformationTypes } from '@/constants'
 import React from 'react'
 
-const AddTransformationTypePage = ({params: {type}} : SearchParamProps) => {
+const AddTransformationTypePage =  ({ params }: SearchParamProps) => {
+  const { type } = params;
   const transformation = transformationTypes[type];
   return (
     <Header title={transformation.title} 
